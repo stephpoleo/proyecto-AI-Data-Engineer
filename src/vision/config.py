904d67f304ace2d22f100c067341008aab7dcdb2
stephpoleo <stephaniepoleo@hotmail.com>
2025-11-14 19:05:04 -0600

    Update constants

--- src/vision/config.py
+++ src/vision/config.py
@@ -2,3 +2,23 @@ CAM_INDEX = 0  # Change index to try different cameras. 0 is usually the default
 FRAME_WIDTH = 640
 FRAME_HEIGHT = 480
 WINDOW_TITLE = "Camera preview"
+
+# YOLO
+YOLO_MODEL_PATH = "models/yolov8n.pt"
+
+ALLOWED_CLASSES = {
+    "person",
+    "backpack",
+    "bottle",
+    "tv",  # OJO: en COCO es "tv", no "tvmonitor"
+    "laptop",
+    "mouse",
+    "remote",
+    "keyboard",
+    "cell phone",
+    "book",
+    "toothbrush",
+}
+
+BOX_COLOR = (255, 0, 0)
+FONT_COLOR = (255, 255, 255)
