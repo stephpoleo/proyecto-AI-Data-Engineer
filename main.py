8df6deb34ebdb7c7e1e76276c5efd78f5c245f8f
stephpoleo <stephaniepoleo@hotmail.com>
2025-11-17 16:37:52 -0600

    Restore classification system line

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from src.etl.batch_etl_system import run_batch_etl_system
 
 if __name__ == "__main__":
     program_mode = ["live_camera", "image", "video"]
-    # run_classification_system(program_mode[1])  # Change index to select mode
+    run_classification_system(program_mode[1])  # Change index to select mode
     run_batch_etl_system()
